a83cb4b8505e2d690c0716e4221ca848ba6d3d65
myOmikron <git@omikron.dev>
2022-07-08 12:10:47 +0200

    Added login template

--- customer/handler.go
+++ customer/handler.go
@@ -5,6 +5,10 @@ import (
 	"net/http"
 )
 
+func Login(c echo.Context) error {
+	return c.Render(http.StatusOK, "login", nil)
+}
+
 func Index(c echo.Context) error {
 	return c.Render(http.StatusOK, "index", nil)
 }
--- customer/router.go
+++ customer/router.go
@@ -2,11 +2,30 @@ package customer
 
 import (
 	"github.com/labstack/echo/v4"
+	"github.com/myOmikron/echotools/middleware"
 	"gorm.io/gorm"
+	"net/http"
 )
 
+func loginRequired(f func(c echo.Context) error) func(c echo.Context) error {
+	return func(c echo.Context) error {
+		sessionContext, err := middleware.GetSessionContext(c)
+		if err != nil {
+			return c.Redirect(http.StatusFound, "/login")
+		}
+
+		if !sessionContext.IsAuthenticated() {
+			return c.Redirect(http.StatusFound, "/login")
+		}
+
+		return f(c)
+	}
+}
+
 func defineRoutes(e *echo.Echo, db *gorm.DB) {
-	e.GET("/", Index)
+	e.GET("/", loginRequired(Index))
+
+	e.GET("/login", Login)
 
 	e.Static("/static", "static/customer/")
 }
